1bc59f29ef2ac1d0ec75be17ce866db88851a503
konifar <yahpeycoy0403@gmail.com>
2018-01-15 02:22:59 +0900

    Changed theme

---
+++ lib/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+final ThemeData themeData = new ThemeData(
+    brightness: Brightness.light,
+    cardColor: Colors.white,
+    dividerColor: Colors.grey[300],
+    backgroundColor: Colors.grey[100],
+    primarySwatch: DroidKaigiColors.theme[500],
+    primaryColor: DroidKaigiColors.theme[500],
+    primaryColorBrightness: Brightness.light,
+    accentColor: DroidKaigiColors.accent[500]);
+
+class DroidKaigiColors {
+  DroidKaigiColors._(); // this basically makes it so you can instantiate this class
+  static const Map<int, Color> theme = const <int, Color>{
+    500: const Color(0xFFFBC812),
+    600: const Color(0xFFFEAD22),
+  };
+
+  static const Map<int, Color> accent = const <int, Color>{
+    500: const Color(0xFFF57C00),
+  };
+}
