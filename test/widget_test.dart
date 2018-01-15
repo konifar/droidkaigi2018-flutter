c8b90b3a7e59af0ae5464dd99c203956886b84c9
konifar <yahpeycoy0403@gmail.com>
2018-01-16 03:17:08 +0900

    Fixed test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,6 @@
 // are correct.
 
 import 'package:droidkaigi2018/ui/app.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -13,17 +12,5 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
     await tester.pump(); // https://github.com/flutter/flutter/issues/1865
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
