f0691fa76dafa21bb7fce2db78aa40b6498fff61
konifar <yahpeycoy0403@gmail.com>
2018-01-15 22:54:13 +0900

    Initialized localization

---
+++ lib/i18n/messages_.dart
@@ -0,0 +1,31 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a  locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+// ignore: unused_element
+final _keepAnalysisHappy = Intl.defaultLocale;
+
+// ignore: non_constant_identifier_names
+typedef MessageIfAbsent(String message_str, List args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  get localeName => '';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => {
+        "about": MessageLookupByLibrary.simpleMessage("詳細"),
+        "allSessions": MessageLookupByLibrary.simpleMessage("すべてのセッション"),
+        "appName": MessageLookupByLibrary.simpleMessage("DroidKaigi 2016"),
+        "map": MessageLookupByLibrary.simpleMessage("マップ"),
+        "mySchedule": MessageLookupByLibrary.simpleMessage("マイスケジュール"),
+        "questionnaire": MessageLookupByLibrary.simpleMessage("アンケート"),
+        "settings": MessageLookupByLibrary.simpleMessage("設定"),
+        "sponsors": MessageLookupByLibrary.simpleMessage("スポンサー")
+      };
+}
