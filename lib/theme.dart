429d25f50efba51f91c38315d6ef64b1015e3855
konifar <yahpeycoy0403@gmail.com>
2018-01-20 23:30:12 +0900

    Modified item layout

--- lib/theme.dart
+++ lib/theme.dart
@@ -18,7 +18,7 @@ final ThemeData themeData = new ThemeData(
 class _MyColors {
   _MyColors._(); // this basically makes it so you can instantiate this class
   static const Map<int, Color> theme = const <int, Color>{
-    500: const Color(0xFFFED44E),
+    500: const Color(0xFFFBC812),
     600: const Color(0xFFFEAD22),
   };
 
--- lib/ui/pages/session_item.dart
+++ lib/ui/pages/session_item.dart
@@ -1,5 +1,7 @@
 import 'package:droidkaigi2018/models/session.dart';
+import 'package:droidkaigi2018/models/speaker.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class SessionsItem extends StatefulWidget {
   SessionsItem(this._session);
@@ -17,24 +19,75 @@ class _SessionsItemState extends State<SessionsItem> {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final TextStyle timeStyle = theme.textTheme.caption;
+    final TextStyle titleStyle = theme.textTheme.title;
+    final TextStyle descriptionStyle = theme.textTheme.caption;
+    final TextStyle speakerNameStyle = theme.textTheme.body2;
+
+    final formatter =
+        new DateFormat.Hm(Localizations.localeOf(context).languageCode);
+    final startAt = formatter.format(_session.startsAt);
+    final endAt = formatter.format(_session.endsAt);
+
     return new Card(
       child: new Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(16.0),
         child: new Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            new Align(
-              alignment: FractionalOffset.centerLeft,
-              child: new CircleAvatar(child: new Text('${_session.title}')),
+            new Text(
+              "$startAt - $endAt / ${_session.room.name}",
+              style: timeStyle,
             ),
-            new Center(
-              child: new Text(_session.title,
-                  style: Theme.of(context).textTheme.title),
+            new Container(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: new Text(_session.title, style: titleStyle)),
+            new DefaultTextStyle(
+              style: descriptionStyle,
+              softWrap: false,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 3,
+              child: new Padding(
+                child: new Text(_session.description),
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              ),
             ),
+            new Text(_session.topic.name, style: descriptionStyle),
+            new Padding(
+              child: new Column(
+                children:
+                    _createSpeakerRows(_session.speakers, speakerNameStyle),
+              ),
+              padding: const EdgeInsets.only(top: 8.0),
+            )
           ],
         ),
       ),
     );
   }
+}
+
+List<Widget> _createSpeakerRows(
+    List<Speaker> speakers, TextStyle speakerNameStyle) {
+  return speakers.map((speaker) {
+    return new Container(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: new Row(
+        children: [
+          new SizedBox(
+            width: 32.0,
+            height: 32.0,
+            child: new CircleAvatar(
+              backgroundImage: new NetworkImage(speaker.profilePicture),
+            ),
+          ),
+          new Container(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: new Text(speaker.fullName, style: speakerNameStyle)),
+        ],
+      ),
+    );
+  }).toList();
 }
